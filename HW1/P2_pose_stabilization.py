cd33f348fef2291eda7350c493cee79e8b495bad
g9zhou <116707042+g9zhou@users.noreply.github.com>
2022-11-16 11:40:47 -0800

    Create P2_pose_stabilization.py

---
+++ HW1/P2_pose_stabilization.py
@@ -0,0 +1,53 @@
+import numpy as np
+from utils import wrapToPi
+
+# command zero velocities once we are this close to the goal
+RHO_THRES = 0.05
+ALPHA_THRES = 0.1
+DELTA_THRES = 0.1
+
+class PoseController:
+    """ Pose stabilization controller """
+    def __init__(self, k1, k2, k3, V_max=0.5, om_max=1):
+        self.k1 = k1
+        self.k2 = k2
+        self.k3 = k3
+
+        self.V_max = V_max
+        self.om_max = om_max
+
+    def load_goal(self, x_g, y_g, th_g):
+        """ Loads in a new goal position """
+        self.x_g = x_g
+        self.y_g = y_g
+        self.th_g = th_g
+
+    def compute_control(self, x, y, th, t):
+        """
+        Inputs:
+            x,y,th: Current state
+            t: Current time (you shouldn't need to use this)
+        Outputs: 
+            V, om: Control actions
+
+        Hints: You'll need to use the wrapToPi function. The np.sinc function
+        may also be useful, look up its documentation
+        """
+        ########## Code starts here ##########
+        dx = self.x_g-x
+        dy = self.y_g-y
+        self.alpha = wrapToPi(np.arctan2(dy,dx)-wrapToPi(th))
+        self.delta = wrapToPi(np.arctan2(dy,dx)-self.th_g)
+        self.rho = np.sqrt(dx**2+dy**2)
+        V = self.k1*self.rho*np.cos(self.alpha)
+        om = self.k2*self.alpha + self.k1*np.sinc(self.alpha/np.pi)*np.cos(self.alpha)*(self.alpha+self.k3*self.delta)
+        self.pub_alpha.publish(self.alpha)
+        self.pub_delta.publish(self.delta)
+        self.pub_rho.publish(self.rho)
+        ########## Code ends here ##########
+
+        # apply control limits
+        V = np.clip(V, -self.V_max, self.V_max)
+        om = np.clip(om, -self.om_max, self.om_max)
+
+        return V, om
